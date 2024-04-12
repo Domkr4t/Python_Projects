0d3e6db977337a94b943c90f95991e1d51044b6b
Ilia <56762351+Domkr4t@users.noreply.github.com>
2024-04-12 19:52:29 +0300

    Update Purchase_forecast_union.py

--- Little_programs/Purchase_forecast_union.py
+++ Little_programs/Purchase_forecast_union.py
@@ -1,15 +1,17 @@
 import pandas as pd
 import os
 import math
+import platform
+import datetime
 import tkinter as tk
 from tkinter import filedialog
-import platform
+from tkinter import ttk
 
 
 sales_file_path = None
 residuals_file_path = None
 report_file_path = None
-
+selected_store = None
 
 #Функция округления
 def custom_ceil(number):
@@ -65,9 +67,26 @@ def load_sales_file():
     print(f"Выбран файл с продажами: {sales_file_path}")
     if sales_file_path:
         message_label_sales.config(text=f"{os.path.basename(sales_file_path)}", foreground='green')
-        message_file_not_upload.config(text=" ")
+
+        # Читаем уникальные значения магазинов из файла
+        sales_data = pd.read_excel(sales_file_path, skiprows=4, usecols=[0])
+
+        sales_data_column_names = ['Магазин']
+        sales_data.columns = sales_data_column_names
+
+        stores = sales_data["Магазин"].unique()
+        stores = [store.strip() for store in stores]
+
+        # Обновляем выпадающий список магазинов
+        store_combobox['values'] = stores[1::]
+        store_combobox.set(stores[1])  # Устанавливаем первый магазин по умолчанию
+
+        message_error.place(x=185, y=70)
+        message_error.config(text=" ")
     else:
         message_label_sales.config(text="Файл не выбран", foreground='red')
+        store_combobox['values'] = ""
+        store_combobox.set("")
         sales_file_path = None
 
 
@@ -78,8 +97,9 @@ def load_residuals_file():
     print(f"Выбран файл с остатками: {residuals_file_path}")
     if residuals_file_path:
         message_label_residuals.config(text=f"{os.path.basename(residuals_file_path)}", foreground='green')
-        message_file_not_upload.config(text=" ")
+        message_error.config(text=" ")
     else:
+        message_error.place(x=185, y=70)
         message_label_residuals.config(text="Файл не выбран", foreground='red')
         residuals_file_path = None
 
@@ -98,22 +118,42 @@ def open_file(file_path):
 # Сохранение файла
 def save_file():
     global report_file_path
+    file_name_start_date = start_date.get()
+    file_name_end_date = end_date.get()
+
+    # Проверка на то что дата пустая строка
+    if not file_name_start_date and not file_name_end_date:
+        file_default_name = f"Закупки_по_{selected_store}"
+    elif not file_name_end_date:
+        file_default_name = f"Закупки_по_{selected_store}_c_{file_name_start_date}"
+    elif not file_name_start_date:
+        file_default_name = f"Закупки_по_{selected_store}_по_{file_name_end_date}"
+    else:
+        file_default_name = f"Закупки_по_{selected_store}_c_{file_name_start_date}_по_{file_name_end_date}"
+
+
     # Открываем диалог выбора файла для сохранения
-    report_file_path = filedialog.asksaveasfilename(defaultextension=".xlsx", title='Сохранить отчет', filetypes=[("Excel Files", "*.xlsx"), ("All Files", "*.*")])
+    report_file_path = filedialog.asksaveasfilename(initialfile=file_default_name,defaultextension=".xlsx", title='Сохранить отчет', filetypes=[("Excel Files", "*.xlsx"), ("All Files", "*.*")])
 
 
 # Создание отчета
 def generate_report():
+    global selected_store
+    global start_date
+    global end_date
 
     # Проверка на заполнение файлов
     if sales_file_path is None and residuals_file_path is None:
-        message_file_not_upload.config(text="Файлы не загружены")
+        message_error.place(x=185, y=70)
+        message_error.config(text="Файлы не загружены")
         return
     elif sales_file_path is None:
-        message_file_not_upload.config(text="Продажи не загружены")
+        message_error.place(x=185, y=70)
+        message_error.config(text="Продажи не загружены")
         return
     elif residuals_file_path is None:
-        message_file_not_upload.config(text="Остатки не загружены")
+        message_error.place(x=185, y=70)
+        message_error.config(text="Остатки не загружены")
         return
 
     # Чтение данных из первого файла (с продажами)
@@ -135,14 +175,37 @@ def generate_report():
     other_second_table = pd.DataFrame(columns=['Номенклатура', 'Заказ'])
 
 
+    selected_store = store_combobox.get()
+
+
+    start_date_str = start_date.get() + " 00:00:00" if start_date.get() else "01.01.1900 00:00:00"
+    end_date_str = end_date.get() + " 23:59:59" if end_date.get() else "31.12.9999 23:59:59"
+    try:
+        start_date_dt = datetime.datetime.strptime(start_date_str, "%d.%m.%Y %H:%M:%S")
+    except ValueError:
+        message_error.place(x=125, y=70)
+        message_error.config(text="Введен неправильный формат начальной даты")
+
+    try:
+        end_date_dt = datetime.datetime.strptime(end_date_str, "%d.%m.%Y %H:%M:%S")
+    except ValueError:
+        message_error.place(x=125, y=70)
+        message_error.config(text="Введен неправильный формат конечной даты")
+
+    start_date_str = start_date_dt.strftime("%d.%m.%Y %H:%M:%S").format()
+    end_date_str = end_date_dt.strftime("%d.%m.%Y %H:%M:%S").format()
+
     # Обработка данных
     for index, row in residuals.iterrows():
 
         nomenclature = row['Номенклатура']
         stock = row['Остаток']
 
-        # Находим все записи в первом файле, соответствующие текущей номенклатуре
-        matching_rows = sales[sales['Номенклатура'] == nomenclature]
+
+        # Находим все записи в файле с продажами, соответствующие текущей магазину и номенклатуре
+        sales_data_filtered_shop = sales[sales['Магазин'] == selected_store]
+        sales_data_filtered_date = sales_data_filtered_shop[(sales_data_filtered_shop['Дата и время'] >= start_date_str) & (sales_data_filtered_shop['Дата и время'] <= end_date_str)]
+        matching_rows = sales_data_filtered_date[sales_data_filtered_date['Номенклатура'] == nomenclature]
 
         # Обработка данных для пива
         if not matching_rows.empty and matching_rows['Категория товара'].str.contains("Пиво").any():
@@ -211,26 +274,43 @@ def generate_report():
         other_results.to_excel(writer, sheet_name="Прочее", index=False)
         other_second_table.to_excel(writer, sheet_name="Прочее", startrow=len(other_results) + 3, index=False)
         message_result = tk.Label(window, text=f"Файл {report_file_path.split('/')[-1]} загружен", foreground='blue')
-        message_result.place(x=185, y=175)
+        message_result.place(x=185, y=240)
 
     open_file (report_file_path)
 
 
 window = tk.Tk()
 window.title("Приложение")
-window.geometry("500x150")
+window.geometry("500x350")
 message_label_sales = tk.Label(window, text="Файл не выбран")
 message_label_sales.place(x=65, y=40)
 message_label_residuals = tk.Label(window, text="Файл не выбран")
 message_label_residuals.place(x=340, y=40)
-message_file_not_upload = tk.Label(window, foreground='red')
-message_file_not_upload.place(x=185, y=60)
+message_error = tk.Label(window, foreground='red')
+
+
+stores = tk.Label(window, text="Магазин")
+stores.place(x=102, y=100)
+store_combobox = ttk.Combobox(window, state="readonly")
+store_combobox.place(x=105, y=120, width=300)
+
+
+start_date_label = tk.Label(window, text="Начальная дата (в формате 29.03.2002)\nБудет поиск c 00:00:00")
+start_date_label.place(x=10, y=175)
+start_date = ttk.Entry(window)
+start_date.place(x=55, y=220)
+
+end_date_label = tk.Label(window, text="Конечная дата (в формате 29.03.2002)\nБудет поиск по 23:59:59")
+end_date_label.place(x=280, y=175)
+end_date = ttk.Entry(window)
+end_date.place(x=320, y=220)
+
 
 btn_sales = tk.Button(window, text="Загрузить файл с продажами", command=load_sales_file)
 btn_residuals = tk.Button(window, text="Загрузить файл с остатками", command=load_residuals_file)
 btn_sales.place(x=25, y=10)
 btn_residuals.place(x=300, y=10)
-btn_generate = tk.Button(window, text="Сгенерировать отчет", command=generate_report)
-btn_generate.place(x=185, y=100)
+btn_generate = tk.Button(window, text="Сгенерировать отчет", command=generate_report, background='#3498db', foreground='#FFFFFF', font='Arial 13 bold', borderwidth=0)
+btn_generate.place(x=105, y=280, height=50, width=300)
 
 window.mainloop()
